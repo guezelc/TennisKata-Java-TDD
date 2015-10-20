f2ca58360f23cf5b515660fc1a80659fdcdf3ffa
GUEZEL Clement <guezelc@PC-B124-07.iut-valence.fr>
2015-10-20 17:42:54 +0200

    Commit after the 2nd test where the server win the first point

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -1,10 +1,22 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
-
+	public static String SCORE_DEFAULT = "Love-all";
+	public static String scoreForJ1 ="Fifteen-love";
+	private String score;
+	
+	public TennisGame()
+	{
+		this.score=SCORE_DEFAULT;
+	}
+	
 	public String getscore() 
 	{
-		return "Love-all";
+		return this.score;
 	}
-
+	
+	public void setscore(String score)
+	{
+		this.score=score;
+	}
 }
--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -6,12 +6,17 @@ import org.junit.Test;
 
 public class TennisGameTest
 {
-	
+	TennisGame game1= new TennisGame() ;
 	@Test
 	public void aSimpleBeginningTest()
 	{
-		TennisGame game1= new TennisGame() ;
 		Assert.assertEquals(game1.getscore(),"Love-all");
 	}
-
+	
+	@Test
+	public void aSecondTestWhereTheServerWinTheFirstPoint()
+	{
+		game1.setscore(TennisGame.scoreForJ1);
+		Assert.assertEquals(game1.getscore(),"Fifteen-love");
+	}
 }
